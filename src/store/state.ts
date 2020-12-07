19fe5e251b2d846150f4967e524917b5f1f256ea
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-12-07 23:53:06 +0500

    add

--- src/store/state.ts
+++ src/store/state.ts
@@ -3,24 +3,39 @@ import { ProductItem } from '../global'
 const INITIAL_STATE: ProductItem[] = [
   {
     id: '123',
-    title: 'Blue t-shirt',
+    title: 'HELLCAT Boys t-shirt',
     description: 'No fancy sizing charts here, one t-shirt size to rule them all',
-    imageUrl: '/blue-tshirt.png',
+    imageUrl: '/pic1.jpg',
     price: 399
   },
   {
     id: '456',
-    title: 'Yellow t-shirt',
+    title: 'DMDM PIG Luminous Short Sleeves  t-shirt',
     description:
       'This unique t-shirt is guaranteed to fit nobody, not even new born babies',
-    imageUrl: '/yellow-tshirt.png',
+    imageUrl: '/pic2.webp',
     price: 499
   },
   {
-    id: '789',
+    id: '749',
+    title: 'Blue t-shirt',
+    description: 'The only product on our site that might actually be worth buying',
+    imageUrl: '/blue-tshirt.png',
+    price: 899
+  },
+  {
+    id: '719',
     title: 'Red t-shirt',
     description: 'The only product on our site that might actually be worth buying',
     imageUrl: '/red-tshirt.png',
+    price: 999
+  },
+
+  {
+    id: '289',
+    title: 'orange t-shirt',
+    description: 'The only product on our site that might actually be worth buying',
+    imageUrl: '/pic2.webp',
     price: 799
   }
 ]
